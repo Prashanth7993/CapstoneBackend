4a9bc6d58514365de878d25b15b75f3c62038a87
devuser09 <devuser09@refabs365.com>
2025-06-19 21:54:35 +0000

    Updated BusServiceApplicationTests.java

--- Backend/bus-service/src/test/java/com/capstone/bus_service/BusServiceApplicationTests.java
+++ Backend/bus-service/src/test/java/com/capstone/bus_service/BusServiceApplicationTests.java
@@ -1,14 +1,17 @@
 package com.capstone.bus_service;
 
+
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-class BusServiceApplicationTests {
-
+class AuthenticationServiceApplicationTests {
 	@Test
 	void contextLoads() {
 		assertTrue(true);
 	}
 
 }
+
+
+
