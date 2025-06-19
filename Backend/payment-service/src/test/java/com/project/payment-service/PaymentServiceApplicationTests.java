d1155b96e4cf927fe10031c79b62ba1f8a65df77
devuser09 <devuser09@refabs365.com>
2025-06-19 21:30:26 +0000

    Added PaymentServiceApplicationTests.java

---
+++ Backend/payment-service/src/test/java/com/project/payment-service/PaymentServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.project.payment_service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class PaymentServiceApplicationTests {
+
+    @Test
+    void dummyTest() {
+        assertTrue(true);
+    }
+}
