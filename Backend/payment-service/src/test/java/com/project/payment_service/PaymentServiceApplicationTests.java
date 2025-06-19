bdba9ac2bf1fd7c429a4810b7f408794a183366e
devuser09 <devuser09@refabs365.com>
2025-06-19 21:27:24 +0000

    Updated PaymentServiceApplicationTests.java

--- Backend/payment-service/src/test/java/com/project/payment_service/PaymentServiceApplicationTests.java
+++ Backend/payment-service/src/test/java/com/project/payment_service/PaymentServiceApplicationTests.java
@@ -1,15 +1,14 @@
 package com.project.payment_service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class PaymentServiceApplicationTests {
 
 	@Test
-	void contextLoads() {
-		assertTrue(true);
-	}
+	void dummyTest() {
+        assertTrue(true);
+    }
 
 }
