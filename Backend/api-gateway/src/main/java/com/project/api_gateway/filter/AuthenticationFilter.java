4f254c52ce873cb1038fb9c8ae468f96d36e2432
devuser09 <devuser09@refabs365.com>
2025-06-22 20:00:39 +0000

    Updated AuthenticationFilter.java

--- Backend/api-gateway/src/main/java/com/project/api_gateway/filter/AuthenticationFilter.java
+++ Backend/api-gateway/src/main/java/com/project/api_gateway/filter/AuthenticationFilter.java
@@ -47,7 +47,7 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
 				try {
 					RestClient restClient = RestClient.create();
 					boolean isValidToken = restClient.get()
-							.uri("http://localhost:9099/auth/validate/token?token=" + authHeaderToken).retrieve()
+							.uri("http://4.177.12.81/auth/validate/token?token=" + authHeaderToken).retrieve()
 							.body(Boolean.class);
 				} catch (Exception e) {
 					exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
