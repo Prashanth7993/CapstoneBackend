943bb1f25143f446ae8882f7607202d3c9a9c477
devuser09 <devuser09@refabs365.com>
2025-06-23 14:12:46 +0000

    Updated CorsConfig.java

--- Backend/authentication-service/src/main/java/com/project/authentication_service/config/CorsConfig.java
+++ Backend/authentication-service/src/main/java/com/project/authentication_service/config/CorsConfig.java
@@ -1,27 +1,27 @@
-package com.project.authentication_service.config;
+// package com.project.authentication_service.config;
 
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.cors.reactive.CorsWebFilter;
+// import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-@Configuration
-public class CorsConfig {
+// @Configuration
+// public class CorsConfig {
 
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-    	 CorsConfiguration corsConfig = new CorsConfiguration();
-         corsConfig.addAllowedOrigin("http://my-global-aks.trafficmanager.net"); 
-         corsConfig.addAllowedMethod("*"); 
-         corsConfig.addAllowedHeader("*"); 
-         corsConfig.setAllowCredentials(true); 
-         corsConfig.addExposedHeader("Authorization"); 
+//     @Bean
+//     public CorsWebFilter corsWebFilter() {
+//     	 CorsConfiguration corsConfig = new CorsConfiguration();
+//          corsConfig.addAllowedOrigin("http://my-global-aks.trafficmanager.net"); 
+//          corsConfig.addAllowedMethod("*"); 
+//          corsConfig.addAllowedHeader("*"); 
+//          corsConfig.setAllowCredentials(true); 
+//          corsConfig.addExposedHeader("Authorization"); 
 
-         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-         source.registerCorsConfiguration("/**", corsConfig); 
+//          UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//          source.registerCorsConfiguration("/**", corsConfig); 
 
-         return new CorsWebFilter(source);
-    }
-}
+//          return new CorsWebFilter(source);
+//     }
+// }
